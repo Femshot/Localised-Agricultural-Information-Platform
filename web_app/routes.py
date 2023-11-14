e1b00b081a21ef861e084923bad457c9ee5870ff
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-11-14 01:02:48 +0100

    Implemented Weather, News & About pages and routes

--- web_app/routes.py
+++ web_app/routes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines routes pages for web-app"""
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from web_app import app
 from web_app.forms import LoginForm
 
@@ -18,3 +18,15 @@ def login():
         flash(f"Login Successful for {form.email.data}", 'success')
         return redirect('/home')
     return render_template('login.html', title=title, form=form)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html', title="Weather Forcast")
