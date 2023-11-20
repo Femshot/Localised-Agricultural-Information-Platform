019a5d768b9448357db85629e9c9b60ed6e50df9
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-11-21 00:19:36 +0100

    increased size of password string for database table column, made copy of nginx server configuration file and edited config.py file to reflect running server settings and modifications

--- web_app/models.py
+++ web_app/models.py
@@ -11,7 +11,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128), nullable=True)
+    password_hash = db.Column(db.String(200), nullable=True)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
